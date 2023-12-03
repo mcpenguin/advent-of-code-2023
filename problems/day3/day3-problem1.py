ff0e4c188ab705b58d24cb9df53ae9e65249e643
Marcus <marcus.chanwc@gmail.com>
2023-12-03 00:47:11 -0500

    day 3

--- problems/day3/day3-problem1.py
+++ problems/day3/day3-problem1.py
@@ -5,26 +5,44 @@ class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.symbol_positions = [] # list of (y, x)
+        self.numbers = [] # (y, x, num_str)
 
-    def process_line(self, line):
+    def is_number_next_to_symbol(self, num_y, num_x, num_str):
+        border = [(num_y-1, x) for x in range(num_x-1, num_x+len(num_str)+1)] + \
+        [(num_y, num_x-1), (num_y, num_x + len(num_str))] + \
+        [(num_y+1, x) for x in range(num_x-1, num_x+len(num_str)+1)]
+        return len(set(border).intersection(set(self.symbol_positions))) > 0
+
+    def process_line(self, line: str, row_idx: int):
         """How to process each line in the input"""
-
-        if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
-        else:
-            self.current_elf_calories += int(line)
-
-    def post_processing(self):
-        """Function that is called after all the lines have been read"""
-        self.elf_calories_list.append(self.current_elf_calories)
+        is_num = False
+        current_num_str = ""
+        current_num_y = row_idx
+        current_num_x = ""
+        for col_idx, c in enumerate(line):
+            if c != '\n':
+                if not c.isdigit() and c != '.':
+                    self.symbol_positions.append((row_idx, col_idx))
+                if c.isdigit():
+                    if not is_num:
+                        is_num = True
+                        current_num_x = col_idx
+                    current_num_str += c
+                if not c.isdigit() and is_num:
+                    self.numbers.append((current_num_y, current_num_x, current_num_str))
+                    current_num_str = ''
+                    is_num = False
+        if is_num == True:
+            self.numbers.append((current_num_y, current_num_x, current_num_str))
 
     def get_solution(self):
+        total = 0
+        for num_y, num_x, num_str in self.numbers:
+            if self.is_number_next_to_symbol(num_y, num_x, num_str):
+                total += int(num_str)
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        return total
 
 # don't change this
 if __name__ == '__main__':
@@ -36,9 +54,9 @@ if __name__ == '__main__':
         f"/inputs/{sys.argv[1]}.txt"
     print("--- DEBUGGING GOES HERE ---")
     with open(filename) as file:
-        for line in file:
-            solution_class.process_line(line)
-    solution_class.post_processing()
+        for row_idx, line in enumerate(file):
+            solution_class.process_line(line, row_idx)
+
     solution = solution_class.get_solution()
     print()
 
--- problems/day3/day3-problem2.py
+++ problems/day3/day3-problem2.py
@@ -1,30 +1,73 @@
 import os
 import sys
+from math import prod
 
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.star_positions = [] # list of (y, x)
+        self.numbers = [] # (y, x, num_str)
 
-    def process_line(self, line):
-        """How to process each line in the input"""
+    def is_number_next_to_gear(self, num_y, num_x, num_str, gear_y, gear_x):
+        border = [(num_y-1, x) for x in range(num_x-1, num_x+len(num_str)+1)] + \
+        [(num_y, num_x-1), (num_y, num_x + len(num_str))] + \
+        [(num_y+1, x) for x in range(num_x-1, num_x+len(num_str)+1)]
+        return (gear_y, gear_x) in border
 
-        if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
+    def get_product_from_star(self, star_y, star_x):
+        border = [(star_y-1, x) for x in range(star_x-1, star_x+2)] + \
+        [(star_y, star_x-1), (star_y, star_x + star_x+1)] + \
+        [(star_y+1, x) for x in range(star_x-1, star_x+2)]
+        numbers_adj_to_star = [
+            n for n in self.numbers
+            if (n[0], n[1]) in border
+        ]
+        if len(numbers_adj_to_star) != 2:
+            print("is not gear")
+            return 0
         else:
-            self.current_elf_calories += int(line)
+            print("is gear")
+            return prod([int(n[2]) for n in numbers_adj_to_star])
 
-    def post_processing(self):
-        """Function that is called after all the lines have been read"""
-        self.elf_calories_list.append(self.current_elf_calories)
+    def process_line(self, line: str, row_idx: int):
+        """How to process each line in the input"""
+        is_num = False
+        current_num_str = ""
+        current_num_y = row_idx
+        current_num_x = ""
+        for col_idx, c in enumerate(line):
+            if c != '\n':
+                if c == '*':
+                    self.star_positions.append((row_idx, col_idx))
+                if c.isdigit():
+                    if not is_num:
+                        is_num = True
+                        current_num_x = col_idx
+                    current_num_str += c
+                if not c.isdigit() and is_num:
+                    self.numbers.append((current_num_y, current_num_x, current_num_str))
+                    current_num_str = ''
+                    is_num = False
+        if is_num == True:
+            self.numbers.append((current_num_y, current_num_x, current_num_str))
 
     def get_solution(self):
+        total = 0
+        numbers_next_to_gear = {} # (gear_y, gear_x): [] 
+        for gear_y, gear_x in self.star_positions:
+            numbers_next_to_gear[(gear_y, gear_x)] = []
+        for num_y, num_x, num_str in self.numbers:
+            for gear_y, gear_x in self.star_positions:
+                if self.is_number_next_to_gear(num_y, num_x, num_str, gear_y, gear_x):
+                    numbers_next_to_gear[(gear_y, gear_x)].append(int(num_str))
+
+        for (num_y, num_x), num_list in numbers_next_to_gear.items():
+            if len(num_list) == 2:
+                total += prod(num_list)
+
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        return total
 
 # don't change this
 if __name__ == '__main__':
@@ -36,9 +79,9 @@ if __name__ == '__main__':
         f"/inputs/{sys.argv[1]}.txt"
     print("--- DEBUGGING GOES HERE ---")
     with open(filename) as file:
-        for line in file:
-            solution_class.process_line(line)
-    solution_class.post_processing()
+        for row_idx, line in enumerate(file):
+            solution_class.process_line(line, row_idx)
+
     solution = solution_class.get_solution()
     print()
 
