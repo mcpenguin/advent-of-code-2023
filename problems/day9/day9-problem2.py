0f441e074d8484cc06342843b72597b9e4df0cfc
Marcus <marcus.chanwc@gmail.com>
2023-12-09 00:20:18 -0500

    day9

--- problems/day9/day9-problem2.py
+++ problems/day9/day9-problem2.py
@@ -1,30 +1,56 @@
 import os
 import sys
+import copy
 
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.seqs = []
+
+    def is_diff_zero(self, diff):
+        return all([x == 0 for x in diff])
+    
+    def get_elemwise_diffs(self, seq):
+        res = []
+        for i in range(0, len(seq)-1):
+            res.append(seq[i+1] - seq[i])
+        return res
+
+    def get_next_e_of_seq(self, seq):
+        diffs = []
+        cur_diff = copy.deepcopy(seq)
+        while not self.is_diff_zero(cur_diff):
+            diffs.append(cur_diff)
+            cur_diff = self.get_elemwise_diffs(cur_diff)
+        diffs.append(cur_diff)
+        
+        diffs = diffs[::-1]
+        # print(seq, diffs)
+        cur_idx = 0
+        diffs[cur_idx] = diffs[cur_idx] + [0]
+        for cur_idx in range(1, len(diffs)):
+            diff = diffs[cur_idx-1][-1]
+            diffs[cur_idx].append(diff + diffs[cur_idx][-1])
+        # print(diffs)
+        # print()
+        return diffs[len(diffs)-1][-1]
 
     def process_line(self, line):
         """How to process each line in the input"""
 
         if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
+            pass
         else:
-            self.current_elf_calories += int(line)
-
-    def post_processing(self):
-        """Function that is called after all the lines have been read"""
-        self.elf_calories_list.append(self.current_elf_calories)
+            self.seqs.append([int(x) for x in line.split(" ")][::-1])
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        print(self.seqs)
+        total = 0
+        for seq in self.seqs:
+            total += self.get_next_e_of_seq(seq)
+        return total
 
 # don't change this
 if __name__ == '__main__':
@@ -38,7 +64,7 @@ if __name__ == '__main__':
     with open(filename) as file:
         for line in file:
             solution_class.process_line(line)
-    solution_class.post_processing()
+
     solution = solution_class.get_solution()
     print()
 
