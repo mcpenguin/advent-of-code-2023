d72e929d00ca23fac79bedc54b88f6fc2ee0523c
Marcus <marcus.chanwc@gmail.com>
2024-12-04 21:41:11 -0800

    day5

--- 2024/problems/day5/day5-problem1.py
+++ 2024/problems/day5/day5-problem1.py
@@ -5,22 +5,57 @@ class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.is_proc_seqs = False
+        self.seqs = []
+        self.orders = []
+        # key: [values], v \in d[k] means that v cannot come before k
+        self.valid = dict[int, set[int]]()
+
+    def update_valid(self, order):
+        after, before = order
+        if after not in self.valid:
+            self.valid[after] = set()
+        if before in self.valid[after]:
+            return
+        
+        self.valid[after].add(before)
+        for next_before in self.valid[after]:
+            self.update_valid([after, next_before])
+
+    def is_seq_valid(self, seq):
+        for i in range(0, len(seq)-1):
+            cur = seq[i]
+            after = set(seq[i+1:])
+            # print(i, cur, after)
+            if not all([cur in self.valid and x in (self.valid[cur]) for x in after]):
+                return False
+        return True
+
 
     def process_line(self, line: str):
         """How to process each line in the input"""
-
         if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
+            self.is_proc_seqs = True
+        elif self.is_proc_seqs:
+            seq = [int(x) for x in line.split(',')]
+            self.seqs.append(seq)
         else:
-            self.current_elf_calories += int(line)
+            order = [int(x) for x in line.split('|')]
+            self.orders.append(order)
+            self.update_valid(order)
+        
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        # print(self.seqs)
+        # print(self.orders)
+        # print(self.valid)
+        result = 0
+        for seq in self.seqs:
+            # print(seq)
+            if self.is_seq_valid(seq):
+                result += seq[len(seq) // 2]
+        return result
 
 # don't change this
 if __name__ == '__main__':
--- 2024/problems/day5/day5-problem2.py
+++ 2024/problems/day5/day5-problem2.py
@@ -5,22 +5,70 @@ class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.is_proc_seqs = False
+        self.seqs = []
+        self.orders = []
+        # key: [values], v \in d[k] means that v cannot come before k
+        self.valid = dict[int, set[int]]()
+
+    def update_valid(self, order):
+        after, before = order
+        if after not in self.valid:
+            self.valid[after] = set()
+        if before in self.valid[after]:
+            return
+        
+        self.valid[after].add(before)
+        for next_before in self.valid[after]:
+            self.update_valid([after, next_before])
+
+    def is_seq_valid(self, seq):
+        for i in range(0, len(seq)-1):
+            cur = seq[i]
+            after = set(seq[i+1:])
+            if not all([cur in self.valid and x in (self.valid[cur]) for x in after]):
+                return False
+        return True
+
 
     def process_line(self, line: str):
         """How to process each line in the input"""
-
         if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
+            self.is_proc_seqs = True
+        elif self.is_proc_seqs:
+            seq = [int(x) for x in line.split(',')]
+            self.seqs.append(seq)
         else:
-            self.current_elf_calories += int(line)
+            order = [int(x) for x in line.split('|')]
+            self.orders.append(order)
+            self.update_valid(order)
+
+    def sort_seq(self, seq):
+        # insertion sort because i'm too lazy
+        # returns True if a < b
+        def lt(a, b):
+            return a in self.valid and b in (self.valid[a])
+        result = []
+        for x in seq:
+            i = 0
+            stop = False
+            while not stop and i < len(result):
+                if lt(x, result[i]):
+                    result.insert(i, x)
+                    stop = True
+                i += 1
+            if not stop:
+                result.append(x)
+        return result
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        result = 0
+        for seq in self.seqs:
+            if not self.is_seq_valid(seq):
+                seq = self.sort_seq(seq)
+                result += seq[len(seq) // 2]
+        return result
 
 # don't change this
 if __name__ == '__main__':
