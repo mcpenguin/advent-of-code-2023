23c9d99b08e5734350a0bc352697786e7694bd04
Marcus <marcus.chanwc@gmail.com>
2024-12-05 21:41:37 -0800

    day6

--- 2024/problems/day6/day6-problem1.py
+++ 2024/problems/day6/day6-problem1.py
@@ -1,45 +1,69 @@
 import os
 import sys
 
+
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.visited_positions = set()
+        self.obstacles = set()
+        self.current_position = (-1, -1)
+        self.row_count = 0
 
-    def process_line(self, line: str):
+    def process_line(self, line: str, row: int):
         """How to process each line in the input"""
-
-        if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
-        else:
-            self.current_elf_calories += int(line)
+        self.row_count += 1
+        self.col_count = len(line)
+        for col, x in enumerate(line):
+            if line[col] == '#':
+                self.obstacles.add((row, col))
+            elif line[col] == '^':
+                self.current_position = (row, col)
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        # print(self.visited_positions)
+        # print(self.obstacles)
+        # print(self.current_position)
+        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        cur_dir_idx = 0
+        print(self.row_count, self.col_count)
+        self.visited_positions.add(self.current_position)
+
+        while self.current_position[0] >= 0 and self.current_position[0] < self.row_count and \
+                self.current_position[1] >= 0 and self.current_position[1] < self.col_count:
+            # print(self.current_position)
+            self.visited_positions.add(self.current_position)
+            cur_dir = dirs[cur_dir_idx % len(dirs)]
+            nextpos = (
+                self.current_position[0] + cur_dir[0], self.current_position[1] + cur_dir[1])
+            while nextpos in self.obstacles:
+                cur_dir_idx += 1
+                cur_dir = dirs[cur_dir_idx % len(dirs)]
+                nextpos = (
+                    self.current_position[0] + cur_dir[0], self.current_position[1] + cur_dir[1])
+            self.current_position = nextpos
+
+        return len(self.visited_positions)
+
 
 # don't change this
 if __name__ == '__main__':
     solution_class = Solution()
-    assert len(sys.argv) > 1, "Please provide the name of the input file in the second terminal argument."
+    assert len(
+        sys.argv) > 1, "Please provide the name of the input file in the second terminal argument."
     filename = os.path.abspath(
         os.path.dirname(os.path.abspath(__file__))
-        ) + \
+    ) + \
         f"/inputs/{sys.argv[1]}.txt"
     print("--- DEBUGGING GOES HERE ---")
     with open(filename) as file:
-        for line in file:
-            solution_class.process_line(line)
-    
+        for pos, line in enumerate(file):
+            solution_class.process_line(line, pos)
+
     solution = solution_class.get_solution()
     print()
 
     print("--- SOLUTION ---")
     print(solution)
-
-        
-
--- 2024/problems/day6/day6-problem2.py
+++ 2024/problems/day6/day6-problem2.py
@@ -1,45 +1,86 @@
 import os
 import sys
+import copy
+
 
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.visited_positions = set()
+        self.obstacles = set()
+        self.original_position = (-1, -1)
+        self.current_position = (-1, -1)
+        self.row_count = 0
 
-    def process_line(self, line: str):
+    def process_line(self, line: str, row: int):
         """How to process each line in the input"""
+        self.row_count += 1
+        self.col_count = len(line)
+        for col, x in enumerate(line):
+            if line[col] == '#':
+                self.obstacles.add((row, col))
+            elif line[col] == '^':
+                self.current_position = (row, col)
+                self.original_position = (row, col)
 
-        if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
-        else:
-            self.current_elf_calories += int(line)
+    def with_new_obstacle_has_loop(self, new_obs_pos):
+        visited_positions_dirs = set()  # tuple, (row, col, dir % 4)
+
+        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        cur_dir_idx = 0
+        self.current_position = self.original_position
+        first = True
+
+        while self.current_position[0] >= 0 and self.current_position[0] < self.row_count and \
+                self.current_position[1] >= 0 and self.current_position[1] < self.col_count:
+
+            visited_positions_dirs.add(
+                (self.current_position[0], self.current_position[1], cur_dir_idx % 4))
+            cur_dir = dirs[cur_dir_idx % len(dirs)]
+            nextpos = (
+                self.current_position[0] + cur_dir[0], self.current_position[1] + cur_dir[1])
+            while nextpos in self.obstacles or nextpos == new_obs_pos:
+                cur_dir_idx += 1
+                cur_dir = dirs[cur_dir_idx % len(dirs)]
+                nextpos = (
+                    self.current_position[0] + cur_dir[0], self.current_position[1] + cur_dir[1])
+
+            self.current_position = nextpos
+            if not first and (self.current_position[0], self.current_position[1], cur_dir_idx % 4) in visited_positions_dirs:
+                return True
+            
+            first = False
+
+        return False
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        count = 0
+        for i in range(self.row_count):
+            for j in range(self.col_count):
+                if self.with_new_obstacle_has_loop((i, j)):
+                    count += 1
+
+        return count
+
 
 # don't change this
 if __name__ == '__main__':
     solution_class = Solution()
-    assert len(sys.argv) > 1, "Please provide the name of the input file in the second terminal argument."
+    assert len(
+        sys.argv) > 1, "Please provide the name of the input file in the second terminal argument."
     filename = os.path.abspath(
         os.path.dirname(os.path.abspath(__file__))
-        ) + \
+    ) + \
         f"/inputs/{sys.argv[1]}.txt"
     print("--- DEBUGGING GOES HERE ---")
     with open(filename) as file:
-        for line in file:
-            solution_class.process_line(line)
-    
+        for pos, line in enumerate(file):
+            solution_class.process_line(line, pos)
+
     solution = solution_class.get_solution()
     print()
 
     print("--- SOLUTION ---")
     print(solution)
-
-        
-
