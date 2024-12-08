e358c6693230907e4d1394b72ce9eaaae72a59bb
Marcus <marcus.chanwc@gmail.com>
2024-12-07 21:39:22 -0800

    day8

--- 2024/problems/day8/day8-problem2.py
+++ 2024/problems/day8/day8-problem2.py
@@ -1,26 +1,59 @@
 import os
 import sys
+from collections import defaultdict
 
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.nodes = defaultdict(list[tuple[int, int]])
+        self.row_idx = 0
 
     def process_line(self, line: str):
         """How to process each line in the input"""
-
-        if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
-        else:
-            self.current_elf_calories += int(line)
+        for col_idx, x in enumerate(line):
+            if x != '\n' and x != '.':
+                self.nodes[x].append((self.row_idx, col_idx))
+        self.row_idx += 1
+        self.num_cols = col_idx
+        
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        self.num_rows = self.row_idx
+        print(self.num_rows, self.num_cols)
+        anode_set = set()
+        for letter, nlist in self.nodes.items():
+            for i in range(len(nlist)):
+                for j in range(i+1, len(nlist)):
+                    x = nlist[i]
+                    y = nlist[j]
+                    # print(letter, x, y)
+                    delta = (x[0] - y[0], x[1] - y[1])
+                    if delta == (0, 0):
+                        anode_set.add(x)
+                    else:
+                        anodes = []
+
+                        c = 0
+                        anode = (x[0] + c * delta[0], x[1] + c * delta[1])
+                        while anode[0] >= 0 and anode[0] < self.num_rows and anode[1] >= 0 and anode[1] < self.num_cols:
+                            anodes.append(anode)
+                            c += 1
+                            anode = (x[0] + c * delta[0], x[1] + c * delta[1])
+
+                        c = 0
+                        anode = (y[0] - c * delta[0], y[1] - c * delta[1])
+                        while anode[0] >= 0 and anode[0] < self.num_rows and anode[1] >= 0 and anode[1] < self.num_cols:
+                            anodes.append(anode)
+                            c += 1
+                            anode = (y[0] - c * delta[0], y[1] - c * delta[1])
+                        
+                        # print(anodes)
+                        for anode in anodes:
+                            anode_set.add(anode)
+        
+        return len(anode_set)
 
 # don't change this
 if __name__ == '__main__':
