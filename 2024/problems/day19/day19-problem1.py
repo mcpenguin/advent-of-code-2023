de15edb37268173776f59ee93a19791b08623192
Marcus <marcus.chanwc@gmail.com>
2024-12-19 13:51:11 +0800

    day19

--- 2024/problems/day19/day19-problem1.py
+++ 2024/problems/day19/day19-problem1.py
@@ -1,26 +1,57 @@
 import os
 import sys
+import re
 
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.parse_designs = False
+        self.designs = []
+        self.patterns = set()
+
+        self.no_work = set()
 
     def process_line(self, line: str):
         """How to process each line in the input"""
 
         if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
+            self.parse_designs = True
         else:
-            self.current_elf_calories += int(line)
+            if self.parse_designs:
+                self.designs.append(line.strip())
+            else:
+                self.patterns = sorted(line.strip().split(', '), key=len, reverse=True)
+                self.pattern = '^(' + '|'.join(line.strip().split(', ')) + ')*$'
+
+    def max_munch(self, s, idx=0):
+        if idx >= len(s):
+            return True
+        if idx in self.no_work:
+            return False
+        
+        # print(idx, s[idx:], self.no_work)
+        for pat in self.patterns:
+            if idx + len(pat) <= len(s) and s[idx:len(pat) + idx] == pat:
+                res = self.max_munch(s, idx + len(pat))
+                if res:
+                    return True
+                
+        self.no_work.add(idx)
+        return False
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        # print(self.patterns)
+        res = 0
+        # print(len(self.designs))
+        for i, design in enumerate(self.designs):
+            self.no_work = set()
+            # if re.match(self.pattern, design):
+            if self.max_munch(design, 0):
+                res += 1
+        return res
+        
 
 # don't change this
 if __name__ == '__main__':
--- 2024/problems/day19/day19-problem2.py
+++ 2024/problems/day19/day19-problem2.py
@@ -1,26 +1,51 @@
 import os
 import sys
+import re
 
 class Solution:
     """Class for solution"""
 
     def __init__(self):
-        self.current_elf_calories = 0
-        self.elf_calories_list = []
+        self.parse_designs = False
+        self.designs = []
+        self.patterns = set()
+        self.num_ways = {}
 
     def process_line(self, line: str):
         """How to process each line in the input"""
 
         if line == '\n':
-            self.elf_calories_list.append(self.current_elf_calories)
-            self.current_elf_calories = 0
+            self.parse_designs = True
         else:
-            self.current_elf_calories += int(line)
+            if self.parse_designs:
+                self.designs.append(line.strip())
+            else:
+                self.patterns = sorted(line.strip().split(', '), key=len, reverse=True)
+
+    def max_munch(self, s, idx=0):
+        if idx >= len(s):
+            return True
+        if idx in self.num_ways:
+            return self.num_ways[idx]
+        
+        num = 0
+        for pat in self.patterns:
+            if idx + len(pat) <= len(s) and s[idx:len(pat) + idx] == pat:
+                res = self.max_munch(s, idx + len(pat))
+                if res:
+                    num += res
+
+        self.num_ways[idx] = num  
+        return num
 
     def get_solution(self):
         """How to retrieve the solution once all lines have been processed"""
-        self.elf_calories_list.sort(reverse=True)
-        return sum(self.elf_calories_list[0:3])
+        res = 0
+        for i, design in enumerate(self.designs):
+            self.num_ways = {}
+            res += self.max_munch(design, 0)
+        return res
+        
 
 # don't change this
 if __name__ == '__main__':
